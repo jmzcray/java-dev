fc9c86c914607002b6ed86a1515dca5855d498d1
Jimz <James.Au@team.telstra.com>
2016-01-22 17:04:43 +1100

    sample matrix rotation continue

--- src/SampleMatrixRotation.java
+++ src/SampleMatrixRotation.java
@@ -25,11 +25,9 @@ public class SampleMatrixRotation {
 
         if (rotate%(((row*2)+(col*2))-4) > 0)
             rotate = rotate%(((row*2)+(col*2))-4);
-
         System.out.println("diameter: " + (((row*2)+(col*2))-4));
         System.out.println("rotate: " + rotate);
 
-        //ArrayList[][] matrix = new ArrayList[row][col];
         List<List<Integer>> matrix = new ArrayList<List<Integer>>();
         for (int r=0; r<row; r++){
             List<Integer> matrixcol = new ArrayList<Integer>();
@@ -39,5 +37,24 @@ public class SampleMatrixRotation {
             matrix.add(matrixcol);
         }
 
+//        int[][] matrixtest = new int[row][col];
+//        matrixtest[0][0] = rotate;
+//        System.out.println("2d array: " + matrixtest[0][0]);
+
+
+        for (int r=0; r<matrix.size(); r++){
+            for (int c=0; c<matrix.get(r).size(); c++){
+
+                int x=r;
+                int y=c;
+                for (int l=0; l<rotate; l++){
+                    if (x=minRow && y<maxCol)
+                        y++;
+                }
+            }
+        }
+
+
     }
+
 }
