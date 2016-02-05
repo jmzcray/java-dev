f20fa80d8bbb41cb1f1808d8b4de8697fa97f63a
Jimz <James.Au@team.telstra.com>
2016-02-05 16:25:31 +1100

    Matrix rotation complete... cheated by looking at a solution in discussion forum :(

--- src/SampleMatrixRotation.java
+++ src/SampleMatrixRotation.java
@@ -9,6 +9,12 @@
  5 6 7 8
  9 10 11 12
  13 14 15 16
+ *
+ * Sample output:
+ 3 4 8 12
+ 2 11 10 16
+ 1 7 6 15
+ 5 9 13 14
  */
 import java.io.*;
 import java.util.*;
@@ -19,42 +25,73 @@ public class SampleMatrixRotation {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int row = in.nextInt();
-        int col = in.nextInt();
+        int rows = in.nextInt();
+        int cols = in.nextInt();
         int rotate = in.nextInt();
 
-        if (rotate%(((row*2)+(col*2))-4) > 0)
-            rotate = rotate%(((row*2)+(col*2))-4);
-        System.out.println("diameter: " + (((row*2)+(col*2))-4));
-        System.out.println("rotate: " + rotate);
+        int[][] arr = new int[rows][cols];
+        int[][] result = new int[rows][cols];
 
-        List<List<Integer>> matrix = new ArrayList<List<Integer>>();
-        for (int r=0; r<row; r++){
-            List<Integer> matrixcol = new ArrayList<Integer>();
-            for (int c=0; c<col; c++) {
-                matrixcol.add(in.nextInt());
+        for (int r=0; r<rows; r++){
+            for (int c=0; c<cols; c++){
+                arr[r][c] = in.nextInt();
             }
-            matrix.add(matrixcol);
+
         }
 
-//        int[][] matrixtest = new int[row][col];
-//        matrixtest[0][0] = rotate;
-//        System.out.println("2d array: " + matrixtest[0][0]);
+        rows--;
+        cols--;
+        for (int r=0; r<=rows; r++) {
+            for (int c=0; c<=cols; c++) {
+                int x = (r < rows - r )? r : rows - r;
+                int y = (c < cols - c )? c : cols - c;
+                int min = (x < y )? x : y;
+                int maxRow = rows - min;
+                int maxCol = cols - min;
+                int diameter = (maxRow + maxCol) * 2 - min * 4;
 
-
-        for (int r=0; r<matrix.size(); r++){
-            for (int c=0; c<matrix.get(r).size(); c++){
-
-                int x=r;
-                int y=c;
-                for (int l=0; l<rotate; l++){
-                    if (x=minRow && y<maxCol)
-                        y++;
+                int row = r;
+                int col = c;
+                for (int a = 0; a < rotate%diameter; a++) {
+                    if (col == min && row < maxRow) {
+                        row++;
+                    }
+                    else if (row == maxRow && col < maxCol) {
+                        col++;
+                    }
+                    else if (row > min && col == maxCol) {
+                        row--;
+                    }
+                    else if (row == min && col > min) {
+                        col--;
+                    }
                 }
+                result[row][col] = arr[r][c];
             }
         }
 
+        for (int r = 0; r <= rows; r++) {
+            for (int c = 0; c <= cols; c++) {
+                System.out.print(result[r][c] + " ");
+            }
+            System.out.println();
+        }
 
+
+/* My failed attempt */
+//        if (rotate%(((row*2)+(col*2))-4) > 0)
+//            rotate = rotate%(((row*2)+(col*2))-4);
+//        System.out.println("diameter: " + (((row*2)+(col*2))-4));
+//        System.out.println("rotate: " + rotate);
+
+//        List<List<Integer>> matrix = new ArrayList<List<Integer>>();
+//        for (int r=0; r<row; r++){
+//            List<Integer> matrixcol = new ArrayList<Integer>();
+//            for (int c=0; c<col; c++) {
+//                matrixcol.add(in.nextInt());
+//            }
+//            matrix.add(matrixcol);
+//        }
     }
 
 }
