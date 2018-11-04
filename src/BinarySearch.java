7c673606e32a6fa310b79a782642d453ed1ee333
Jimz Au <james.au@valtech.com>
2018-11-04 19:58:59 +1100

    implemented iterative binary search and timing search results

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,30 +1,56 @@
 public class BinarySearch {
 
-    int binarySearch(int[] arr, int start, int length, int target) {
+    int recursiveBinarySearch(int[] arr, int start, int length, int target) {
         if (length > 0) {
-            int index = start + (length / 2);
-            if (arr[index] == target) {
-                return index;
-            } else if (arr[index] < target) {
-                return binarySearch(arr, ++index, arr.length - index, target);
+            int mid = start + (length / 2);
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                return recursiveBinarySearch(arr, ++mid, arr.length - mid, target);
             } else {
-                return binarySearch(arr, start, index - start, target);
+                return recursiveBinarySearch(arr, start, mid - start, target);
             }
         }
         return -1;
+    }
+
+    int iterativeBinarySearch(int[] arr, int start, int length, int target) {
+
+        while(length > 0) {
+            int mid = start + (length / 2);
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+                length = arr.length - start;
+            } else {
+                length = mid - start;
+            }
+        }
+        return -1;
+
     }
 
     // Driver method to test above
     public static void main(String args[])
     {
         BinarySearch ob = new BinarySearch();
-        int arr[] = {2,3,4,10,40};
+        int result;
+        long startTime, endTime;
+        int arr[] = {2,3,4,10,40,50,100};
         int length = arr.length;
-        int target = 1;
-        int result = ob.binarySearch(arr,0,length-1, target);
-        if (result == -1)
-            System.out.println("Element not present");
-        else
-            System.out.println("Element found at index " + result);
+        int target = 40;
+
+        startTime = System.nanoTime();
+        result = ob.recursiveBinarySearch(arr,0,length-1, target);
+        endTime = System.nanoTime();
+        System.out.println("recursiveBinarySearch took " + (endTime - startTime) + "ms and result is: " +
+                ((result == -1) ? "Element not present" : "Element found at index " + result));
+
+        startTime = System.nanoTime();
+        result = ob.iterativeBinarySearch(arr,0,length-1, target);
+        endTime = System.nanoTime();
+        System.out.println("iterativeBinarySearch took " + (endTime - startTime) + "ms and result is: " +
+                ((result == -1) ? "Element not present" : "Element found at index " + result));
     }
 }
