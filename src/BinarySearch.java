45f3c0401bd874ed66a50ba715fa429f7b9b413f
Jimz Au <james.au@valtech.com>
2018-11-04 19:27:29 +1100

    implemented recursive binary search

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,14 +1,17 @@
 public class BinarySearch {
 
     int binarySearch(int[] arr, int start, int length, int target) {
-        int index = start + (length / 2);
-        if (arr[index] == target) {
-            return index;
-        } else if (arr[index] < target) {
-            return binarySearch(arr, index, arr.length-index, target);
-        } else {
-            return binarySearch(arr, start, index, target);
+        if (length > 0) {
+            int index = start + (length / 2);
+            if (arr[index] == target) {
+                return index;
+            } else if (arr[index] < target) {
+                return binarySearch(arr, ++index, arr.length - index, target);
+            } else {
+                return binarySearch(arr, start, index - start, target);
+            }
         }
+        return -1;
     }
 
     // Driver method to test above
@@ -17,7 +20,7 @@ public class BinarySearch {
         BinarySearch ob = new BinarySearch();
         int arr[] = {2,3,4,10,40};
         int length = arr.length;
-        int target = 40;
+        int target = 1;
         int result = ob.binarySearch(arr,0,length-1, target);
         if (result == -1)
             System.out.println("Element not present");
