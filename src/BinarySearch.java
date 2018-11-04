31b00bb833d2b150f1d18cb382f8df88b429482b
Jimz Au <james.au@valtech.com>
2018-11-04 18:34:25 +1100

    ini implement of binary search

---
+++ src/BinarySearch.java
@@ -0,0 +1,27 @@
+public class BinarySearch {
+
+    int binarySearch(int[] arr, int start, int length, int target) {
+        int index = start + (length / 2);
+        if (arr[index] == target) {
+            return index;
+        } else if (arr[index] < target) {
+            return binarySearch(arr, index, arr.length-index, target);
+        } else {
+            return binarySearch(arr, start, index, target);
+        }
+    }
+
+    // Driver method to test above
+    public static void main(String args[])
+    {
+        BinarySearch ob = new BinarySearch();
+        int arr[] = {2,3,4,10,40};
+        int length = arr.length;
+        int target = 40;
+        int result = ob.binarySearch(arr,0,length-1, target);
+        if (result == -1)
+            System.out.println("Element not present");
+        else
+            System.out.println("Element found at index " + result);
+    }
+}
